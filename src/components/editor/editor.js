de2f65de58818e128aa51984f89ca6c1a10630e8
Sachin <jadhavsachin174@gmail.com>
2023-07-18 09:46:27 +0530

    Add comment to explain more about the header buttons

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -15,6 +15,29 @@ function Editor() {
     setSelectedQueryIndex(index);
   }, []);
 
+  /*
+    TO-DO: to make the app header interactive we can implement following methods and pass it to Header component.
+    const newQueryHandler = () => {};
+     - we can add new entry to the inputqueris variable to keep track of all the tabs and their respective data
+
+
+    const clearQueryHandler = () => {};
+     - we will clear the input box
+
+
+    const runQueryHandler = () => {};
+     - we will read the query and pass it to the enginer and wait for the output. Once we get the output we can update the queryResults
+       or handle the error/exception and show it inside the output section
+
+  */
+
+  /*
+    TO-DO: to make the app input section interactive we can add useState variable to save the typed query and
+    implement following method and pass it to Input component.
+
+    const changeQueryHandler = () => {};
+  */
+
   return (
     <>
       <Header />
